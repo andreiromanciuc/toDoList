9c51631599a468cdee7c31e4ca2609eb811e6a93
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-01-15 21:34:49 +0200

    Added first line in SQL

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,24 @@
 package org.fasttrackit;
 
+import org.fasttrackit.persistance.TaskRepository;
+import org.fasttrackit.transfer.CreateTaskRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, SQLException {
+        CreateTaskRequest createTaskRequest = new CreateTaskRequest();
+        createTaskRequest.setDescription("Learn JDBS");
+        createTaskRequest.setDeadline(LocalDate.now().plusWeeks(1));
+
+        TaskRepository taskRepository = new TaskRepository();
+        taskRepository.createTask(createTaskRequest);
     }
 }
