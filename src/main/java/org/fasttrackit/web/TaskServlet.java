d47d5e915ab87da5b2ff4f38cc532169ad7a5554
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-02-05 21:17:44 +0200

    configured CORS

--- src/main/java/org/fasttrackit/web/TaskServlet.java
+++ src/main/java/org/fasttrackit/web/TaskServlet.java
@@ -27,7 +27,7 @@ public class TaskServlet extends HttpServlet {
     // endpoint
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        setAccessControlHeaders(resp);
         CreateTaskRequest request = ObjectMapperConfiguration.objectMapper
                 .readValue(req.getReader(), CreateTaskRequest.class);
 
@@ -42,6 +42,7 @@ public class TaskServlet extends HttpServlet {
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setAccessControlHeaders(resp);
         String id = req.getParameter("id");
 
         try {
@@ -53,6 +54,8 @@ public class TaskServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setAccessControlHeaders(resp);
+
         String id = req.getParameter("id");
 
         UpdateTaskRequest updateTaskRequest = ObjectMapperConfiguration.objectMapper
@@ -67,6 +70,7 @@ public class TaskServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setAccessControlHeaders(resp);
 
 
         try {
@@ -79,6 +83,18 @@ public class TaskServlet extends HttpServlet {
             resp.sendError(500, "Internal Server Error: "+e.getMessage());
         }
 
+    }
 
+    // for pre-flight request
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setAccessControlHeaders(resp);
+
+    }
+// CORS configuration (Cross-Origin-Resource-Sharing)
+    private void setAccessControlHeaders(HttpServletResponse resp) {
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        resp.setHeader("Access-Control-Allow-Headers", "content-type");
     }
 }
